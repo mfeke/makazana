62813c47355f0fb9404cfc02378c48b242ec959e
mfeke <mfekethohulane@gail.com>
2025-08-27 20:37:20 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms'; 
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { SortPipe } from './sort.pipe';
+import { NumberLoginComponent } from './components/acounts/number-login/number-login.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SortPipe,
+    NumberLoginComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
