8ad6c43710def193324d3aec98f4da66f566811b
mfeke <mfekethohulane@gail.com>
2025-09-01 20:19:40 +0200

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  imageUrl = 'https://i.postimg.cc/KYkDWC5Z/mens-home-6-410x487.jpg';
+  imageUrl = '';
+
+  imgs = [
+    {name:"graphics", image:"https://i.postimg.cc/x1DS4SQR/graphics-home-4-410x487.jpg"},
+    {name:"men's", image:"https://i.postimg.cc/KYkDWC5Z/mens-home-6-410x487.jpg"},
+    {name:"short", image:"https://i.postimg.cc/hv3Ld9mM/shorts-home-4-410x487.jpg"}
+  ]
 }
