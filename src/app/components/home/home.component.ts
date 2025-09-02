c45ddca73af21808f4d8a4ebdf49bc27f785295c
mfeke <mfekethohulane@gail.com>
2025-09-02 11:19:09 +0200

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,10 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   imageUrl = '';
-
+  name = 'Throwie T-Shirt';
+  price = '$48.92';
   imgs = [
-    {name:"graphics", image:"https://i.postimg.cc/x1DS4SQR/graphics-home-4-410x487.jpg"},
-    {name:"men's", image:"https://i.postimg.cc/KYkDWC5Z/mens-home-6-410x487.jpg"},
-    {name:"short", image:"https://i.postimg.cc/hv3Ld9mM/shorts-home-4-410x487.jpg"}
-  ]
+    {
+      name: 'graphics',
+      image: 'https://i.postimg.cc/x1DS4SQR/graphics-home-4-410x487.jpg',
+    },
+    {
+      name: "men's",
+      image: 'https://i.postimg.cc/KYkDWC5Z/mens-home-6-410x487.jpg',
+    },
+    {
+      name: 'short',
+      image: 'https://i.postimg.cc/hv3Ld9mM/shorts-home-4-410x487.jpg',
+    },
+  ];
 }
